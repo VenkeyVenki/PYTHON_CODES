3006b2254ef048113f916deeb217fd311c05faa7
Venkatesh M N <venkateshmn1124@gmail.com>
2026-03-11 21:24:40 +0530

    Add files via upload

---
+++ Guess_Number_Game.py
@@ -0,0 +1,4 @@
+import random
+n=random.randint(1,10)
+g=int(input("Enter the number:"))
+print("Correct" if g==n else "Wrong")
---
+++ Random_Password_Generator.py
@@ -0,0 +1,2 @@
+import random,string
+print("".join(random.choice(string.ascii_letters) for i in range(8)))
