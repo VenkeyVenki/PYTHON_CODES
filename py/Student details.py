27a2062855c264fe76142fe05550beffa6e01eee
Venkatesh M N <venkateshmn1124@gmail.com>
2026-03-23 14:45:15 +0530

    Add files via upload

---
+++ py/Student details.py
@@ -0,0 +1,14 @@
+name=input("Enter Name:")
+USN=input("Enter USN:")
+sub1=int(input("Enter sub1 marks:"))
+sub2=int(input("Enter sub2 marks:"))
+sub3=int(input("Enter sub3 marks:"))
+print("Name is",name)
+print("USN is",USN)
+print("Sub1 marks is",sub1)
+print("Sub2 marks is",sub2)
+print("Sub3 marks is",sub3)
+t=sub1+sub2+sub3
+print("Total marks is",t)
+per=(t/300)*100
+print("Persentage is",per)
